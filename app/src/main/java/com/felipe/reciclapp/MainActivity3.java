e7a21c47d0d07a7834264772065f8eeb24210371
DanierTrochez <yenifernataly2003@gmail.com>
2023-04-20 17:27:01 -0500

    fix

--- app/src/main/java/com/felipe/reciclapp/MainActivity3.java
+++ app/src/main/java/com/felipe/reciclapp/MainActivity3.java
@@ -7,44 +7,28 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity3 extends AppCompatActivity {
-    ListView list;
+   Spinner spinner;
+   String[] valores = new String[]{"Colombia","Chile","Ecuador","Peru","Brasil",};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        list=(ListView)findViewById(R.id.listaseleccion);
-        cargarlista();
+       spinner = (Spinner) findViewById(R.id.spinner);
+       ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,valores);
+       spinner.setAdapter((adapter));
+       spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+               Toast.makeText(getApplicationContext(), "pais"+ (String) spinner.getItemAtPosition(position),  Toast.LENGTH_SHORT).show();
+           }
+       });
     }
 
-    private void cargarlista() {
-        String[] valores = new String[]{"CHILE","PERU","ARGENTINA","COLOMBIA"};
 
-        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,valores);
-        list.setAdapter(adapter);
-        list.setOnClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
-                    case 0:
-                        Toast.makeText(MainActivity3.this, "PRECIONO CHILE", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 1:
-                        Toast.makeText(MainActivity3.this, "PRECIONO CHILE", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 2:
-                        Toast.makeText(MainActivity3.this, "PRECIONO CHILE", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 3:
-                        Toast.makeText(MainActivity3.this, "PRECIONO CHILE", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                }
-            }
-        });
-    }
 }
